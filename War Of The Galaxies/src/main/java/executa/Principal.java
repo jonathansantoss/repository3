1e93c3a64ec5f2f58f2fb2685be47cb6f9e24a82
jonathansantoss <jonathan.gomess1@gmail.com>
2018-10-29 12:08:52 -0300

    ajustes

--- War Of The Galaxies/src/main/java/executa/Principal.java
+++ War Of The Galaxies/src/main/java/executa/Principal.java
@@ -3,11 +3,10 @@ package executa;
 import Jogo.Janelas;
 
 public class Principal {
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		Janelas janela = new Janelas();
-		// janela.executa();
+		//janela.executa();
 
 	}
 
