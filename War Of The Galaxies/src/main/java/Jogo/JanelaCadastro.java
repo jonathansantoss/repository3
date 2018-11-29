0465bb3c1e7e928e3e427482cb55f4af61ee4cb9
jonathansantoss <jonathan.gomess1@gmail.com>
2018-11-29 13:41:01 -0200

    aajustes

--- War Of The Galaxies/src/main/java/Jogo/JanelaCadastro.java
+++ War Of The Galaxies/src/main/java/Jogo/JanelaCadastro.java
@@ -1,10 +1,8 @@
 package Jogo;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -19,28 +17,27 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
 import javax.swing.table.DefaultTableModel;
 
 import DAO.CadastroDAO;
 
 @SuppressWarnings("serial")
 public class JanelaCadastro extends JPanel implements MouseListener {
-	
+
 	public int score;
 	public String nome;
-	
+
 	private CadastroDAO cadastroDAO;
 	private Image fundo;
-	
+
 	private JButton btnNovoJogo;
 	private JButton btnEntrar;
 	private JButton btnRecords;
 	private JButton btnInfo;
 	private JButton btnVoltar;
-	
+
 	private JLabel nomes;
-	
+
 	private JTextField txtNome;
 	private JTable tabela;
 
@@ -49,7 +46,7 @@ public class JanelaCadastro extends JPanel implements MouseListener {
 	public JanelaCadastro() {
 		configuracao();
 	}
-	
+
 	public void mouseClicked(MouseEvent e) {
 		if (e.getSource() == btnNovoJogo) {
 			btnNovoJogo.setVisible(false);
@@ -57,26 +54,22 @@ public class JanelaCadastro extends JPanel implements MouseListener {
 			btnInfo.setVisible(false);
 			btnEntrar.setVisible(true);
 			txtNome.setVisible(true);
-		}
-		else if (e.getSource() == btnEntrar) {
+		} else if (e.getSource() == btnEntrar) {
 			cadastroFase = true;
 			nome = txtNome.getText();
 			cadastroDAO.save(nome);
-		}
-		else if (e.getSource() == btnRecords) {
+		} else if (e.getSource() == btnRecords) {
 			btnNovoJogo.setVisible(false);
 			btnRecords.setVisible(false);
 			btnInfo.setVisible(false);
 			tabela();
-		}
-		else if (e.getSource() == btnInfo) {
+		} else if (e.getSource() == btnInfo) {
 			btnNovoJogo.setVisible(false);
 			btnRecords.setVisible(false);
 			btnInfo.setVisible(false);
 			btnVoltar.setVisible(true);
 			desenvolvedores(true);
-		}
-		else if (e.getSource() == btnVoltar) {
+		} else if (e.getSource() == btnVoltar) {
 			btnNovoJogo.setVisible(true);
 			btnRecords.setVisible(true);
 			btnInfo.setVisible(true);
@@ -108,39 +101,39 @@ public class JanelaCadastro extends JPanel implements MouseListener {
 	public boolean isCadastroFase() {
 		return cadastroFase;
 	}
-	
+
 	public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics g2d =  g.create();
-        g2d.fillOval(100, 100, 200, 200);
-        g2d.drawImage(fundo, 0, 0, null);
-        g2d.dispose(); 
-    }
-	
-	public void configuracao() {		
+		super.paintComponent(g);
+		Graphics g2d = g.create();
+		g2d.fillOval(100, 100, 200, 200);
+		g2d.drawImage(fundo, 0, 0, null);
+		g2d.dispose();
+	}
+
+	public void configuracao() {
 		ImageIcon ref = new ImageIcon("Imagens-Jogo\\fundo-Cadastro.jpeg");
 		fundo = ref.getImage();
-		
+
 		cadastroDAO = new CadastroDAO();
 
 		btnNovoJogo = new JButton("Novo Jogo");
 		btnNovoJogo.setBounds(10, 10, 100, 100);
-		
+
 		btnRecords = new JButton("Records");
 		btnRecords.setBounds(100, 100, 100, 100);
-		
+
 		btnVoltar = new JButton("Voltar");
-		btnVoltar.setBounds(100,100,100,100);
+		btnVoltar.setBounds(100, 100, 100, 100);
 		btnVoltar.setVisible(false);
-		
+
 		btnInfo = new JButton("Informações");
 		btnInfo.setBounds(100, 100, 100, 100);
-		
+
 		btnEntrar = new JButton("Entrar");
 		btnEntrar.setBounds(100, 100, 100, 100);
 		btnEntrar.setVisible(false);
-		
-		txtNome = new JTextField(); 
+
+		txtNome = new JTextField();
 		txtNome.setVisible(false);
 		txtNome.setBounds(10, 10, 10, 10);
 
@@ -154,143 +147,138 @@ public class JanelaCadastro extends JPanel implements MouseListener {
 
 		GridBagConstraints regras = new GridBagConstraints();
 		GridBagLayout grid = new GridBagLayout();
-	
+
 		setLayout(grid);
-		
+
 		regras.fill = GridBagConstraints.NONE;
 		regras.anchor = GridBagConstraints.CENTER;
 		regras.weightx = 4;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 100, 200);
-		add(btnNovoJogo,regras);
-		
+		add(btnNovoJogo, regras);
+
 		regras.anchor = GridBagConstraints.SOUTH;
 		regras.weightx = 7;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 60, 200);
-		add(btnRecords,regras);
-		
+		add(btnRecords, regras);
+
 		regras.anchor = GridBagConstraints.SOUTH;
 		regras.weightx = 7;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 20, 200);
-		add(btnInfo,regras);
-		
+		add(btnInfo, regras);
+
 		regras.anchor = GridBagConstraints.NORTHEAST;
 		regras.gridx = 1;
 		regras.gridy = 1;
-		regras.weightx= 5;
+		regras.weightx = 5;
 		regras.insets = new Insets(270, 270, 0, 450);
-		add(btnEntrar,regras);
-		
+		add(btnEntrar, regras);
+
 		regras.anchor = GridBagConstraints.SOUTH;
 		regras.gridx = 1;
 		regras.gridy = 1;
-		regras.weightx= 5;
+		regras.weightx = 5;
 		regras.insets = new Insets(270, 270, 0, 450);
-		add(btnVoltar,regras);
-       
+		add(btnVoltar, regras);
+
 		regras.fill = GridBagConstraints.HORIZONTAL;
 		regras.gridx = 1;
 		regras.gridy = 1;
-		regras.weightx= 2;
-		regras.weighty= 2;
+		regras.weightx = 2;
+		regras.weighty = 2;
 		regras.insets = new Insets(275, 400, 0, 540);
 		add(txtNome, regras);
 	}
-	
-	
+
 	public void desenvolvedores(boolean visivel) {
-		
+
 		GridBagConstraints regras = new GridBagConstraints();
 		Font fonte = new Font("arial", Font.BOLD, 20);
-		
+
 		nomes = new JLabel("DESENVOLVEDORES");
 		nomes.setFont(fonte);
 		nomes.setForeground(new Color(255, 140, 0));
-		if (visivel == true) nomes.setVisible(true);
-		else nomes.setVisible(false);
-		
+		if (visivel != true)
+			nomes.setVisible(false);
+
 		regras.anchor = GridBagConstraints.CENTER;
 		regras.weightx = 4;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 180, 200);
-		add(nomes,regras);
-		
-		
+		add(nomes, regras);
+
 		nomes = new JLabel("FABIANO ALBINO FERREIRA");
 		nomes.setFont(fonte);
 		nomes.setForeground(new Color(255, 140, 0));
 		nomes.setVisible(false);
-		
+
 		regras.anchor = GridBagConstraints.CENTER;
 		regras.weightx = 4;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 100, 200);
-		add(nomes,regras);
-		
-		
+		add(nomes, regras);
+
 		nomes = new JLabel("HELTON CARLOS DE MOURA");
 		nomes.setFont(fonte);
 		nomes.setForeground(new Color(255, 140, 0));
-		
+
 		regras.anchor = GridBagConstraints.CENTER;
 		regras.weightx = 4;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 60, 200);
-		add(nomes,regras);
-		
+		add(nomes, regras);
+
 		nomes = new JLabel("JONATHAN GOMES DOS SANTOS");
 		nomes.setFont(fonte);
 		nomes.setForeground(new Color(255, 140, 0));
-		
+
 		regras.anchor = GridBagConstraints.CENTER;
 		regras.weightx = 4;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, 20, 200);
-		add(nomes,regras);
-		
-		
+		add(nomes, regras);
+
 		nomes = new JLabel("VALDECIR MUNHAM JUNIOR");
 		nomes.setFont(fonte);
 		nomes.setForeground(new Color(255, 140, 0));
-		
+
 		regras.anchor = GridBagConstraints.CENTER;
 		regras.weightx = 4;
 		regras.gridx = 1;
 		regras.gridy = 1;
 		regras.insets = new Insets(0, 200, -20, 200);
-		add(nomes,regras);
-		 
+		add(nomes, regras);
+
 	}
-	
-	
+
 	@SuppressWarnings("deprecation")
 	public void tabela() {
 		LinkedList<Cadastro> cadastros = new LinkedList<Cadastro>();
 		cadastros = cadastroDAO.buscar();
-		String colunas[] = {"Nome","Score"};
+		String colunas[] = { "Nome", "Score" };
 
-		tabela = new JTable(cadastros.size(),2);
+		tabela = new JTable(cadastros.size(), 2);
 		tabela.enable(false);
-		
+
 		DefaultTableModel modelo = new DefaultTableModel(colunas, 0);
-		
-		for (Cadastro cadastro : cadastros) 
-			modelo.addRow(new String[] {cadastro.nome,String.valueOf(cadastro.score)});
-		
+
+		for (Cadastro cadastro : cadastros)
+			modelo.addRow(new String[] { cadastro.nome, String.valueOf(cadastro.score) });
+
 		GridBagConstraints regrasTabela = new GridBagConstraints();
 		regrasTabela.anchor = GridBagConstraints.NORTHEAST;
 		regrasTabela.gridx = 1;
 		regrasTabela.gridy = 1;
-		regrasTabela.weightx= 10;
+		regrasTabela.weightx = 10;
 		regrasTabela.insets = new Insets(70, 70, 0, 450);
 
 		tabela.setModel(modelo);
